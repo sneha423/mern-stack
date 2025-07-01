997878de90d625575204f9306d189fd03ec722e7
sneha423 <sgupta10_be23@thapar.edu>
2025-07-01 12:04:15 +0530

    new file on async projects

--- async.js
+++ async.js
@@ -1,4 +1,8 @@
 //two types of code in js
 // blocking and non blocking
 // blocking:read file sync
+
 // non blocking:read file async
+// for asynchronus :
+// we have web api/Node
+// in this we will learn about: fetch,register call back,promise queue,task queue
