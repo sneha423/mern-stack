9a32474963328c72b6535d766bfb81f7424f3617
sneha423 <sgupta10_be23@thapar.edu>
2025-07-04 21:56:28 +0530

    new file(classes)

---
+++ classes.js
@@ -0,0 +1,49 @@
+class user{
+    //we can add properties and functions in class
+    constructor(username,email,password){
+        this.username=username
+        this.email=email
+        this.password=password
+    }
+    encryptPassword(){
+        return (`${this.password}ajivwv`)
+    }
+    capitalizeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+const xyz=new user('sneha','egowukfview;.com',2640)
+console.log(xyz.encryptPassword());
+console.log(xyz.capitalizeUsername());
+
+//behind the scene:
+//we will create a function user with all th atributes and then add the remaining functions using prototype
+
+//inheritance using class
+class users{
+    constructor(username){
+        this.username=username
+    }
+    logMe(){
+        console.log(`username is ${this.username}`);
+        
+    }
+}
+class teacher extends users{
+    constructor(username,email,password){
+        super(username)//direct access to username no need for this or special call this is the shortcut in class
+        this.email=email
+        this.password=password
+    }
+    addCourse(){
+        console.log(`a new course was added by ${this.username}`);
+        
+    }
+}
+const name=new teacher('amita','lhwcgi asl.com',6320)
+name.addCourse()
+name.logMe()
+const newuser=new users('sumil')
+// newuser.addCourse()//no access
+newuser.logMe()
+console.log(newuser===name);
