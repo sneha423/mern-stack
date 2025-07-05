d677eac45f907ea2ed7e5c21b5e4243a0f929ee8
sneha423 <sgupta10_be23@thapar.edu>
2025-07-05 15:18:32 +0530

    changes made

--- objects.js
+++ objects.js
@@ -2,98 +2,94 @@
 //object literals are not singleton
 //Object.create(constructor method)
 
-
 //object literals construction:
-const mysym=Symbol('mykey');
-const user={
-    name:'sneha',
-    age:'21',
-    loc:'jaipur',
-    [mysym]:'mykey1',
-    "full name":'sneha gupta'
-}
+const mysym = Symbol("mykey");
+const user = {
+  name: "sneha",
+  age: "21",
+  loc: "jaipur",
+  [mysym]: "mykey1",
+  "full name": "sneha gupta",
+};
 console.log(`name is ${user.name} and age is ${user.age}`);
-console.log(user["full name"]);//remember syntax with sq bracks
-console.log(user[mysym]);//for accessing symbol
-console.log(typeof(user.mysym));
+console.log(user["full name"]); //remember syntax with sq bracks
+console.log(user[mysym]); //for accessing symbol
+console.log(typeof user.mysym);
 
 //to overwrite values
-user.age=23;
+user.age = 23;
 //Object.freeze(user);//now we can't change the value of any property of user
 console.log(user);
 
 //functions in js
-user.greeting=function(){
-    console.log('hello user!!');
-    
-}
+user.greeting = function () {
+  console.log("hello user!!");
+};
 console.log(user.greeting());
 console.log(user.greeting);
-user.greetingtwo=function(){
-    console.log(`hello ${this.name}`);
-    
-}
+user.greetingtwo = function () {
+  console.log(`hello ${this.name}`);
+};
 console.log(user.greetingtwo());
 
 // const tinderUser=new Object()//singleton object
 // const tinderUser={}//non singleton object
 
 //object within an object
-const regUser={
-    email:'iwefvb.gmail.com',
-    fullname:{
-        userFullName:{
-            first_name:'amita',
-            last_name:'gupta'
-        }
-    }
-}
+const regUser = {
+  email: "iwefvb.gmail.com",
+  fullname: {
+    userFullName: {
+      first_name: "amita",
+      last_name: "gupta",
+    },
+  },
+};
 console.log(regUser.fullname.userFullName.first_name);
 
 //combining objects
-const obj1={1:'a',2:'b'};
-const obj2={3:'a',4:'b'};
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
 //const obj3={obj1,obj2}//will give object in object
 //const obj3=Object.assign(obj1,obj2);//will merge the objects
-const obj3=Object.assign({},obj1,obj2);//guaranteed result
+const obj3 = Object.assign({}, obj1, obj2); //guaranteed result
 //to spread objects:
-const obj4={...obj1,...obj2,...obj3};
+const obj4 = { ...obj1, ...obj2, ...obj3 };
 console.log(obj3);
 console.log(obj4);
 
 //array of objects(seen in database):
-const arrObj=[
-    {
-        id:1,
-        email:'wegbv.com'
-    },
-    {
-        id:2,
-        email:'dvvrrv.com'
-    },
-    {
-        id:1,
-        email:'wefrf3ebv.com'
-    }  
-]
+const arrObj = [
+  {
+    id: 1,
+    email: "wegbv.com",
+  },
+  {
+    id: 2,
+    email: "dvvrrv.com",
+  },
+  {
+    id: 1,
+    email: "wefrf3ebv.com",
+  },
+];
 console.log(arrObj[1].email);
 //IMP:
-console.log(Object.keys(obj2));//will return an array
+console.log(Object.keys(obj2)); //will return an array
 console.log(Object.values(obj2));
-console.log(Object.entries(obj2));//array inside an array
-console.log(obj1.hasOwnProperty('3'));
-
+console.log(Object.entries(obj2)); //array inside an array
+console.log(obj1.hasOwnProperty("3"));
 
 //de-structuring of objects:
-const course={
-    name:'efbbqw;',
-    price:8934,
-    courseTeacher:'h2e4f2lk'
-}
+const course = {
+  name: "efbbqw;",
+  price: 8934,
+  courseTeacher: "h2e4f2lk",
+};
 //another way to call values(used in react)
-const {courseTeacher:instructor}=course;
+const { courseTeacher: instructor } = course;
 // console.log(courseTeacher);
-console.log(instructor);//another way
+console.log(instructor); //another way
 
 //in react we will use
 // const navbar=({companyName})=>{
@@ -116,3 +112,29 @@ console.log(instructor);//another way
 //     {},
 //     {}
 // ]
+
+//behind the scene of const keyword
+//object has its own properties and we can change them
+//like if we are doing writable false then we will not be able to change its value later and if we do enumerable false then we can;t iterate on that
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(descripter);
+//we can define these properties for our own object also
+const myuser = {
+  name: "sneha",
+  pssword: 3252,
+  getName() {
+    console.log("name is printed");
+  },
+};
+// console.log(myuser);
+console.log(Object.getOwnPropertyDescriptor(myuser, "name"));
+Object.defineProperty(myuser, "name", {
+  writable: false,
+  enumerable: false,
+});
+console.log(Object.getOwnPropertyDescriptor(myuser, "name"));
+for (let [key, value] of Object.entries(myuser)) {
+  if (typeof value !== "function") {
+    console.log(`${key} : ${value}`);
+  }
+}
