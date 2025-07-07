25db3ba3bf449bb15eb59bafe424c2de3c9c9cac
sneha423 <sgupta10_be23@thapar.edu>
2025-07-07 20:54:44 +0530

    syntax of html in jsx done

--- react/01viteReact/src/App.jsx
+++ react/01viteReact/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import Basic from './basics';
 function App() {
   const [count, setCount] = useState(0)
-
+  const username='sneha gupta'
   return (
     <>
     <Basic/>
-    <h1>hey!!</h1>
+    <h1>hey!! {username}</h1>
+    {/* the thing in curly braces is evaluated expression */}
     </>
     
    
--- react/01viteReact/src/main.jsx
+++ react/01viteReact/src/main.jsx
@@ -1,27 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
-function MyApp(){
-  return(
+import App from "./App.jsx";
+function MyApp() {
+  return (
     <div>
       <h1>custom function</h1>
     </div>
-  )
+  );
 }
 //object can't be used as such
-const ReactElemnet = {
-  type: "a",
-  props: {
+// const ReactElemnet = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
+);
+const anotherUser='amita'
+//this is the react syntax of adding an element
+//we first add basic html,then waht we want to display and then variables
+const ReactElemnet = React.createElement(
+  "a",
+  {
     href: "https://google.com",
     target: "_blank",
   },
-  children: "click me to visit google",
-};
-
-const anotherElement=(
-  <a href="https://google.com" target='_blank'>visit google</a>
-)
-createRoot(document.getElementById('root')).render(
-  anotherElement
-)
+  "click to go to google",
+  anotherUser
+);
+createRoot(document.getElementById("root")).render(
+  ReactElemnet 
+);
