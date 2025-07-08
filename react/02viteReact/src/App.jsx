22a9b12a45ad68e174ff7458ce757bd476090a62
sneha423 <sgupta10_be23@thapar.edu>
2025-07-08 14:11:36 +0530

    hooks basics

--- react/02viteReact/src/App.jsx
+++ react/02viteReact/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
+import { useState } from 'react'//these are hooks
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //syntax for hook:
+  //hook gives array in output in 0 pos there is a variable and in 1st pos there is a function which is responsible to change the value of variable
+  let [counter,setCounter]=useState(10)
+  let [message,setmessage]=useState('')
+  // let counter=15
+  const addValue=()=>{
+    // console.log('value added',Math.random());
+    // counter++//this is not happening directly on the ui updation
+    counter++
+    if(counter>20){
+      setmessage('cant add futhur')
+      
+    }
+    else{
+    setCounter(counter)
+    console.log(counter);
+    setmessage('')
+    }
+    
+  }
+  const removeValue=()=>{
+    counter--
+    if(counter<0){
+      setmessage('cannot decrease furthur')
+      
+    }
+    else{
+    setCounter(counter)
+    console.log(counter);
+    setmessage('')
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>learning hooks</h1>
+    <h2>couter value:{counter}</h2>
+    <button onClick={addValue}>add value</button>
+    <br />
+    <button onClick={removeValue}>remove value</button>
+    <p>{message}</p>
     </>
+   
   )
 }
 
