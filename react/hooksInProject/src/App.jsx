8fbb3f5976c467e377a199eb5a6fe17039e76b57
sneha423 <sgupta10_be23@thapar.edu>
2025-07-09 16:16:56 +0530

    commit

--- react/hooksInProject/src/App.jsx
+++ react/hooksInProject/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-
+import './App.css'
 
 function App() {
   const [length, setlength] = useState(8);
