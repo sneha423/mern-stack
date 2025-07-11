d702935ff541a73d174265ebf10f130865867da0
sneha423 <sgupta10_be23@thapar.edu>
2025-07-11 16:49:30 +0530

    commit

--- react/currencyConverter/src/App.jsx
+++ react/currencyConverter/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-
+import { useState ,useId} from 'react'
+import { InputBox } from './components'
+import useCurrencyInfo from './customHooks/useCurrencyInfo'
 import './App.css'
 import './index.css'
 function App() {
   
-
+  const [amount,setAmount]=useState(0)
+  const [from,setFrom]=useState('usd')
+  const [to, setTo]=useState('inr')
+  const [convertedAmount,setConvertedAmount]=useState(0 )
+  const currencyInfo=useCurrencyInfo(from)
   return (
     <>
       <h1 className='text-3xl bg-orange-500'>currency app</h1>
