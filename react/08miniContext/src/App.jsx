7982d322da8fbdff7f122ff49a3bbaea2c85e592
sneha423 <sgupta10_be23@thapar.edu>
2025-07-12 20:30:21 +0530

    commit

--- react/08miniContext/src/App.jsx
+++ react/08miniContext/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import userContextProvider from './Context/UserContextProvider'
 function App() {
   
 
   return (
-    <>
+    <userContextProvider>
       <h1>mini context api project</h1>
-    </>
+    </userContextProvider>
   )
 }
 
