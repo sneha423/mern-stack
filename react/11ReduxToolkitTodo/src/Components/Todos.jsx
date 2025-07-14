c7fe4ec3cb9b8a362e45336dc1413e1bd58acbdd
sneha423 <sgupta10_be23@thapar.edu>
2025-07-14 14:59:48 +0530

    save

--- react/11ReduxToolkitTodo/src/Components/Todos.jsx
+++ react/11ReduxToolkitTodo/src/Components/Todos.jsx
@@ -5,11 +5,14 @@ function Todos() {
     const data=useSelector((state)=>state.todos)//holds array
     const dispatch=useDispatch()
   return (
+    <>
     <div>Todos</div>
     {data.map((todo)=>(
-        <li key={todo.id}>{todo.text}</li>
-        <button onClick={()}></button>
+        <li key={todo.id}>{todo.text}
+        <button onClick={()=>dispatch.removeTodo(todo.id)}></button>
+        </li>
     ))}
+    </>
   )
 }
 
