dded0996bfdcc6c17d17fb6d6e96be8e241317d4
sneha423 <sgupta10_be23@thapar.edu>
2025-07-14 12:28:34 +0530

    save

--- react/11ReduxToolkitTodo/src/Components/AddTodo.jsx
+++ react/11ReduxToolkitTodo/src/Components/AddTodo.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import{useDispatch} from 'react-redux'
+import { addTodo } from '../Features/todo/todoSlice'
 function AddTodo() {
+    const [input,setInput]=useState('')
+    const dispatch=useDispatch()
+    const addTodoHandler=(e)=>{
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
   return (
      <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
--- react/11ReduxToolkitTodo/src/Components/Todos.jsx
+++ react/11ReduxToolkitTodo/src/Components/Todos.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { useSelector,useDispatch } from 'react-redux'
+import { removeTodo } from '../Features/todo/todoSlice'
 function Todos() {
+    const data=useSelector((state)=>state.todos)//holds array
+    const dispatch=useDispatch()
   return (
     <div>Todos</div>
+    {data.map((todo)=>(
+        <li key={todo.id}>{todo.text}</li>
+        <button onClick={()}></button>
+    ))}
   )
 }
 
