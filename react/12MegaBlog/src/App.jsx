430028f4406b11dc0af05110261faf92476e21a9
sneha423 <sgupta10_be23@thapar.edu>
2025-07-15 20:25:53 +0530

    commit

--- react/12MegaBlog/src/App.jsx
+++ react/12MegaBlog/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { logIn, logOut } from "./Store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,8 +24,17 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div className="min-h-sc"></div>
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        {/* <main>
+          <Outlet/>
+        </main> */}
+        <Footer/>
+      </div>
+    </div>
   ):null
+  
 }
 
 export default App;
