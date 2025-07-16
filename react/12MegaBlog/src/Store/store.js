0c4185eaab06b9a29fd27b60469cae20ffe5e87d
sneha423 <sgupta10_be23@thapar.edu>
2025-07-16 22:06:24 +0530

    commit

--- react/12MegaBlog/src/Store/store.js
+++ react/12MegaBlog/src/Store/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
+import{authReducer} from ''
 const store=configureStore({
-    reducer:{}
+    reducer:{
+        auth:authReducer
+    }
 })
 export default store
--- react/12MegaBlog/src/components/index.js
+++ react/12MegaBlog/src/components/index.js
@@ -1,3 +1,4 @@
+//we are storing all exponents in one place and exporting them through here
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
