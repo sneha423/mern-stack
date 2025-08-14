56298c3de9e5a19d471e86a800a0e6ff779aa4fc
sneha423 <sgupta10_be23@thapar.edu>
2025-08-15 00:32:32 +0530

    mongodb connected

---
+++ Backend/mondoDB/vidtube/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
+const connectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+        console.log(`\n mongodb connected! DB host:${connectionInstance.connection.host}`);
+        
+    }catch(error){
+        console.log('mongodb connection error',error);
+        process.exit(1)
+        
+    }
+}
+export default connectDB
--- Backend/mondoDB/vidtube/src/index.js
+++ Backend/mondoDB/vidtube/src/index.js
@@ -1,1 +1,16 @@
-console.log('hello backend course');
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./.env",
+});
+const PORT = process.env.PORT || 8001;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mogodb connection error", err);
+  });
