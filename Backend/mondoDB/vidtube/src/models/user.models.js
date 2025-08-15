2f4c8065280e478aa1b38807e800c8f6dc6ce49f
sneha423 <sgupta10_be23@thapar.edu>
2025-08-15 21:08:54 +0530

    methods in models

--- Backend/mondoDB/vidtube/src/models/user.models.js
+++ Backend/mondoDB/vidtube/src/models/user.models.js
@@ -8,6 +8,8 @@
 // createdAt Date
 // updatedAt Date
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     //id will be genrated automatically by mongodb
@@ -44,8 +46,33 @@ const userSchema = new Schema(
         ref: "Video",
       },
     ],
+    refreshtoken: {},
   },
-  { timestamps: true }//for createdat and updatedat
+  { timestamps: true } //for createdat and updatedat
 );
+//methods in mongoose
+//1. encrypt the password
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordcorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  //short lived access token
+  jwt.sign(
+    { _id: this._id, email: this.email, username: this.username },
+    process.env.ACCESS_TOKEN_SECRET,{expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+  );
+}; 
+userSchema.methods.generateRefeshToken = function () {
+  //short lived access token
+  jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_SECRET,{expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+  );
+}; 
 export const User = mongoose.model("User", userSchema); //good practice to store the user in upper pattern and singlular
 //   with this we can import the model anytime and it will also give access to database features like query selection etc.
