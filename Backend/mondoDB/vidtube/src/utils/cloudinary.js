e333ab779c9f5c5d3c010e48616f9323d548b7f9
sneha423 <sgupta10_be23@thapar.edu>
2025-08-15 22:41:32 +0530

    handling files in mern

---
+++ Backend/mondoDB/vidtube/src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "node";
+//configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response=cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
+    console.log('file uploaded on cloudinary File src:'+response.url);
+    //once thefile is uploaded we want to delete it from our server
+    fs.unlinkSync(localFilePath)
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+export { uploadOnCloudinary };
